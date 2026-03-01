51ff0c027860b8bf07e6b0efa9005efff36d25c9
vladislavprozorov <slawasam@mail.ru>
2026-02-28 18:56:15 -0800

    feat: add CLI entry point with commander

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('CLI bootstrap')
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+    .name("component-forge")
+    .description("Architecture-first CLI for scalable React projects")
+    .version("0.1.0")
+
+program.parse(process.argv);
